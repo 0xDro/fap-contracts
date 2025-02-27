7b00cd50639438655864722cc1dc5ec2e97dad39
0xDro <alegala21@gmail.com>
2025-02-26 21:58:54 -0700

    remappings

--- src/fap.sol
+++ src/fap.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.20;
 
-import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 
 contract Fap is ReentrancyGuard {
     // Constants for deposit limits and time windows
